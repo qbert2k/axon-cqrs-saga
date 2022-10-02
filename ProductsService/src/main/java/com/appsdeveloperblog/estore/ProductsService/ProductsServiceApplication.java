923618c3086c129f7bb8b74ac07e1b1184cd9ebf
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-10-02 16:56:56 +0100

    Validation: Message Dispatch Interceptor.

--- ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/ProductsServiceApplication.java
+++ ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/ProductsServiceApplication.java
@@ -1,8 +1,12 @@
 package com.appsdeveloperblog.estore.ProductsService;
 
+import com.appsdeveloperblog.estore.ProductsService.command.interceptors.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +16,8 @@ public class ProductsServiceApplication {
         SpringApplication.run(ProductsServiceApplication.class, args);
     }
 
+    @Autowired
+    public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
+        commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
+    }
 }
